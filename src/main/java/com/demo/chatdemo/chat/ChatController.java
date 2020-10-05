b25e64396ed1a046d50193fbe727af50f42495f9
jungyoon huh <gjwjddbsg@gmail.com>
2020-10-05 22:15:22 +0900

    feat ⚡️ project init

---
+++ src/main/java/com/demo/chatdemo/chat/ChatController.java
@@ -0,0 +1,26 @@
+package com.demo.chatdemo.chat;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        log.info(message.getMessage());
+        if (MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+
+}
---
+++ src/main/java/com/demo/chatdemo/chat/ChatMessage.java
@@ -0,0 +1,26 @@
+package com.demo.chatdemo.chat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage implements Serializable {
+    // 메시지 타입 : 입장, 채팅
+    private MessageType type; // 메시지 타입
+    private String roomId; // 방번호
+    private String sender; // 메시지 보낸사람
+    private String message; // 메시지
+}
+
+enum MessageType {
+    ENTER, TALK
+}
